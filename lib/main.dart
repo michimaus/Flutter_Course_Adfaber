9cc91610dd34b5838b3f458c475a411e8692be41
mihnea <mihnea.duican@softbinator.com>
2021-07-02 02:48:10 +0300

    Lesson 2 setup.

--- lib/main.dart
+++ lib/main.dart
@@ -24,9 +24,9 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      // home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
       // home: RegisterScreen(),
-      home: LoginScreen(),
+      // home: LoginScreen(),
     );
   }
 }
