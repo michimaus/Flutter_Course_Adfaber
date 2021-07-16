3c1cb4fa3df91cc4b71b3502e7a097689c9509cf
mihnea <mihnea.duican@softbinator.com>
2021-07-16 10:03:25 +0300

    Recorded lesson 4.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,32 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:lectia2/screens/login_screen.dart';
+import 'package:lectia2/screens/main_screen.dart';
 import 'package:lectia2/screens/register_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  MyApp.preferences = await SharedPreferences.getInstance();
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
+  static late SharedPreferences preferences;
+
   @override
   Widget build(BuildContext context) {
+    Widget _firstScreen = LoginScreen();
+
+    if (preferences.getString('userEmail') != null) {
+      _firstScreen = MainScreen();
+    }
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -25,8 +42,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // home: MyHomePage(title: 'Flutter Demo Home Page'),
-      // home: RegisterScreen(),
-      home: LoginScreen(),
+      home: _firstScreen,
     );
   }
 }
